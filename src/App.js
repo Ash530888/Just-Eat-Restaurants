cff18341b80d10320e54014e96d101cc06def0c7
Ash530888 <aishah.s.qureshi@gmail.com>
2025-03-31 18:43:02 +0100

    table

--- src/App.js
+++ src/App.js
@@ -28,9 +28,34 @@ function App() {
 
   return (
     <div>
-    <p>{JSON.stringify(restaurants)}</p>
-    
-      </div>
+
+<table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisines</th>
+            <th>Rating</th>
+            <th>Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {restaurants.length > 0 ? (
+            restaurants.map((restaurant, index) => (
+              <tr key={index}>
+                <td>{restaurant.name}</td>
+                <td>{restaurant.cuisines.map(cuisine => cuisine.name).join(", ")}</td>
+                <td>{restaurant.rating.starRating} ({restaurant.rating.count})</td>
+                <td>{restaurant.address.city}, {restaurant.address.firstLine}, {restaurant.address.postalCode}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="4">No restaurants found.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
